e1bc693790c35410a3cd49b678a072b6ad0699c3
ChenZhipu <66401899+ChenZhipu@users.noreply.github.com>
2021-01-22 18:25:21 -0500

    Feature update.
    Hospital selection feature start.
    Mode selection UI start.

---
+++ COVIDMAPII/Assets/COVID Map II/Script/AnimateTo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.Geospatial;
+using Microsoft.Maps.Unity;
+
+public class AnimateTo : MonoBehaviour
+{
+    MapSceneOfLocationAndZoomLevel BaycrestHealthSciences = new MapSceneOfLocationAndZoomLevel(new LatLon(43.729967, -79.434236), 17.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    [SerializeField]
+    private MapRenderer _map = null;
+
+    private void Awake()
+    {
+        Debug.Assert(_map != null);
+    }
+
+    public void AnimateCentreTo()
+    {
+        _map.SetMapScene(BaycrestHealthSciences);
+    }
+}
